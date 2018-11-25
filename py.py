1071b56f618ddcc2dbb4a5ff6aed850445f3a310
ELRedivo <elredivo@gmail.com>
2018-11-25 02:19:54 -0200

    Practive MOD4...

--- py.py
+++ py.py
@@ -1,2 +1,16 @@
-# Practice MOD4-1
+# [ ] print a string that outputs the following exactly: The new line character is "\n"
+
 print('The new line character is \"\\n\"')
+
+# [ ] print output that is exactly (with quotes): "That's how we escape!"
+
+print('"That\'s how we escape!"')
+
+# [ ] with only 1 print statement and using No Space Characters, output the text commented below  
+
+# 1       one
+# 22      two
+# 333     three
+
+print('#','1' + '\t' + 'one' + '\n' + '#','22' + '\t' + 'two' + '\n' + '#','333' + '\t' + 'three')
+
