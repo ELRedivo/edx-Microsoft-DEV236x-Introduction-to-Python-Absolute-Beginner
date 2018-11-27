640e59962408625b2362cfb657cc9dbd905c2fe8
ELRedivo <elredivo@gmail.com>
2018-11-27 13:00:21 -0200

    Practice 4 - tickets

--- py.py
+++ py.py
@@ -1,1 +1,18 @@
-#
+# [ ] review the code, run, fix the error
+
+tickets = int(input("Enter tickets remaining (0 to quit): "))
+
+#################### WHILE #######################################
+while tickets != 0:
+    
+    # if tickets are multiple of 3 then "winner"
+    if int(tickets/3) == tickets/3:
+        print("You win!")
+        break
+    else:
+        print("Sorry, not a winner.")
+    
+    tickets = int(input("Enter tickets remaining (0 to quit): "))
+##################################################################
+
+print("Game ended.")
